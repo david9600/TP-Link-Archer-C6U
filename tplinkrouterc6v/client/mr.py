e97f11a69b0c621d3cb4358c0bc51fff6ba8f4a5
david9600 <david.c.arnold@icloud.com>
2026-08-12 21:56:32 +1000

    Adjust logging.

--- tplinkrouterc6v/client/mr.py
+++ tplinkrouterc6v/client/mr.py
@@ -268,8 +268,10 @@ class TPLinkMRClientBase(AbstractRouter):
                         self._logger.info(activ_intf)
                         self._logger.info('enabled wan intfs: %s', enabled_wan_intfs)
                         for intf in self._to_list(enabled_wan_intfs):
+                            self._logger.info(intf)
                             if intf.get('name') == activ_intf:
-                                self._logger.info(intf.get_ipv6('X_TP_ExternalIPv6Address'))
+                                pass
+                                #self._logger.info(intf.get_ipv6('X_TP_ExternalIPv6Address'))
                                 #status.wan_ipv6_enabled = item.get('X_TP_IPv6Enabled')
                                 #status._wan_ipv4_addr = item.get_ip('externalIPAddress')
                                 #status._wan_ipv6_addr = item.get_ipv6('X_TP_ExternalIPv6Address')
