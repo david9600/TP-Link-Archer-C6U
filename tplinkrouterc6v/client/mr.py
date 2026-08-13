6a55e299c2088070f6c1bbb88ef88a01f362651f
david9600 <david.c.arnold@icloud.com>
2026-08-13 23:26:23 +1000

    Fix variable name.

--- tplinkrouterc6v/client/mr.py
+++ tplinkrouterc6v/client/mr.py
@@ -253,7 +253,7 @@ class TPLinkMRClientBase(AbstractRouter):
                 _, wan_aux_values = self.req_act(wan_aux_acts)
                 if wan_aux_values:
                     for item in self._to_list(wan_aux_values):
-                        if int(item['enable']) == 0 and values.__class__ == list:
+                        if int(item['enable']) == 0 and wan_aux_values.__class__ == list:
                             continue
                         status.wan_ipv6_enabled = bool(int(item.get('X_TP_IPv6Enabled', '0')))
                         status._wan_ipv6_addr = get_ipv6(item.get('X_TP_ExternalIPv6Address', '::'))
