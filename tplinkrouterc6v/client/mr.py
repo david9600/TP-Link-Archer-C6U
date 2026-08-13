4b8bf398d3ac19f327727f50433ccf888d559b06
david9600 <david.c.arnold@icloud.com>
2026-08-13 20:59:17 +1000

    Fix indents.

--- tplinkrouterc6v/client/mr.py
+++ tplinkrouterc6v/client/mr.py
@@ -266,20 +266,20 @@ class TPLinkMRClientBase(AbstractRouter):
                         self.ActItem(self.ActItem.GET, 'L3_FORWARDING', attrs=['__ifAliasName']), 
                         self.ActItem(self.ActItem.GET, 'L3_IP6_FORWARDING', attrs=['__ifAliasName']),
                     ]
-                _, wan_fwd_values = self.req_act(wan_fwd_acts)
-                self._logger.info('wan forwarding values: %s', wan_fwd_values)
-                # To keep it simple, assume same active interface for v4 & v6
-                activ_intf = wan_fwd_values.get('1').get('__ifAliasName')
-                self._logger.info('active intf: %s', activ_intf)
-                self._logger.info('enabled wan intfs: %s', enabled_wan_intfs)
-                for intf in self._to_list(enabled_wan_intfs):
-                    self._logger.info('intf is %s', intf)
-                    if intf.get('name') == activ_intf:
-                        self._logger.info('name matches activ_intf')
-                        status.wan_ipv6_enabled = bool(int(intf.get('X_TP_IPv6Enabled', '0')))
-                        #self._logger.info('ipv6 addr is %s', intf.get_ipv6('X_TP_ExternalIPv6Address'))
-                        #status._wan_ipv4_addr = item.get_ip('externalIPAddress')
-                        status._wan_ipv6_addr = intf.get_ipv6('X_TP_ExternalIPv6Address', '::')
+                    _, wan_fwd_values = self.req_act(wan_fwd_acts)
+                    self._logger.info('wan forwarding values: %s', wan_fwd_values)
+                    # To keep it simple, assume same active interface for v4 & v6
+                    activ_intf = wan_fwd_values.get('1').get('__ifAliasName')
+                    self._logger.info('active intf: %s', activ_intf)
+                    self._logger.info('enabled wan intfs: %s', enabled_wan_intfs)
+                    for intf in self._to_list(enabled_wan_intfs):
+                        self._logger.info('intf is %s', intf)
+                        if intf.get('name') == activ_intf:
+                            self._logger.info('name matches activ_intf')
+                            status.wan_ipv6_enabled = bool(int(intf.get('X_TP_IPv6Enabled', '0')))
+                            #self._logger.info('ipv6 addr is %s', intf.get_ipv6('X_TP_ExternalIPv6Address'))
+                            #status._wan_ipv4_addr = item.get_ip('externalIPAddress')
+                            status._wan_ipv6_addr = intf.get_ipv6('X_TP_ExternalIPv6Address', '::')
                 except:
                     self._logger.info('exception 2')
         
