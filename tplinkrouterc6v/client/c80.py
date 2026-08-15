29bde3fa4204b53951ebb14a8aadc845a6e80910
david9600 <david.c.arnold@icloud.com>
2026-08-15 11:56:42 +1000

    Remove default from status field.

--- tplinkrouterc6v/client/c80.py
+++ tplinkrouterc6v/client/c80.py
@@ -209,14 +209,14 @@ class TplinkC80Router(AbstractRouter):
 
         if self._ipv6_support:
             ipv6_request_text = '#'.join([
-                '77|1,0,0',
+                IPV6_WAN_REQUEST,
             ])
             data_blocks = self._return_data_block(ipv6_request_text)
             self._logger.info('data_blocks = %s', data_blocks)
             if data_blocks:
                 ipv6_wan_info = self._parse_last_values_from_block(data_blocks.get('45|1,0,0', []))
                 self._logger.info('ipv6_wan_info is %s', ipv6_wan_info) 
-                status.wan_ipv6_enabled = int(ipv6_wan_info.get('status', '')) != 0
+                status.wan_ipv6_enabled = ipv6_wan_info.get('status') != '0'
                 status._wan_ipv6_addr = get_ipv6(ipv6_wan_info.get('globalIp', '::'))
             else:
                 self._ipv6_support = False
@@ -260,14 +260,14 @@ class TplinkC80Router(AbstractRouter):
 
         if self._ipv6_support:
             ipv6_request_text = '#'.join([
-                '75|1,0,0',
+                IPV6_WAN_REQUEST,
             ])
             data_blocks = self._return_data_block(ipv6_request_text)
             self._logger.info('data_blocks = %s', data_blocks)
             if data_blocks:
                 ipv6_wan_info = self._parse_last_values_from_block(data_blocks.get('45|1,0,0', []))
                 self._logger.info('ipv6_wan_info is %s', ipv6_wan_info) 
-                status.wan_ipv6_enabled = int(ipv6_wan_info.get('status', '')) != 0
+                status.wan_ipv6_enabled = ipv6_wan_info.get('status') != '0'
                 status._wan_ipv6_addr = get_ipv6(ipv6_wan_info.get('globalIp', '::'))
             else:
                 self._ipv6_support = False
