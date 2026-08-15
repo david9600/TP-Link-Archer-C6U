147a62c02302dc71d554fca24927b4e4ec0e335e
david9600 <david.c.arnold@icloud.com>
2026-08-15 11:34:43 +1000

    Remove reference to 'site'.

--- tplinkrouterc6v/client/c80.py
+++ tplinkrouterc6v/client/c80.py
@@ -251,8 +251,7 @@ class TplinkC80Router(AbstractRouter):
             self._logger.info('data_blocks = %s', data_blocks)
             if data_blocks:
                 ipv6_wan_info = self._parse_last_values_from_block(data_blocks.get('45|1,0,0', []))
-                self._logger.info('ipv6_wan_info is %s', ipv6_wan_info)
-                self._logger.info('ipv6_site_info is %s', ipv6_site_info)  
+                self._logger.info('ipv6_wan_info is %s', ipv6_wan_info) 
                 status.wan_ipv6_enabled = int(ipv6_wan_info.get('status'), '') != 0
                 status._wan_ipv6_addr = get_ipv6(ipv6_wan_info.get('globalIp', '::'))
             else:
