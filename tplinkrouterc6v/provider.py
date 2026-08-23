1fac5c7f8ed13a65f91f045bfa431efe67807177
david9600 <david.c.arnold@icloud.com>
2026-08-23 22:11:21 +1000

    Adjust log message.

--- tplinkrouterc6v/provider.py
+++ tplinkrouterc6v/provider.py
@@ -37,7 +37,7 @@ class TplinkRouterProvider:
                 continue
             router = client(host, password, username, logger, verify_ssl, timeout)
             if router.supports():
-                logger.info('TplinkRouterProvider: supports() succeeded for %s, client chosen: %s', host, client.__name__)
+                logger.info('TplinkRouterProvider: supports() returned True for %s, client chosen: %s', host, client.__name__)
                 return router
             elif logger is not None:
                 logger.debug('TplinkRouterProvider: supports() failed for %s (%s)', host, client.__name__)
