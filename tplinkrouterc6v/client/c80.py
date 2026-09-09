f4646362a07150f75a5bf943ce6144ad0cf5adec
david9600 <david.c.arnold@icloud.com>
2026-09-09 18:46:40 +1000

    Try request id 21.

--- tplinkrouterc6v/client/c80.py
+++ tplinkrouterc6v/client/c80.py
@@ -297,9 +297,10 @@ class TplinkC80Router(AbstractRouter):
         self.request(1, 0, True, data=body)
 
     def set_ewan_connect(self, enable: bool) -> None:
-        enable_string = f'status {int(enable)}'
-        wan_ip_request = "23|1,0,0"
-        text = f'id {wan_ip_request}\r\n{enable_string}'
+        enable_string = f'enable {int(enable)}'
+        wan_ip_request = "21|1,0,0"
+        text = f'id {wan_ip_request}'
+        # text = f'id {wan_ip_request}\r\n{enable_string}'
         body = self._encrypt_body(text)
         self.request(0, 0, True, data=body)
 
