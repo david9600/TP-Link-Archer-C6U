84212ad232674ae7564bf9170601fbb6ae9cede8
david9600 <david.c.arnold@icloud.com>
2026-09-09 17:48:33 +1000

    Add method set_ewan_connect.

--- tplinkrouterc6v/client/c80.py
+++ tplinkrouterc6v/client/c80.py
@@ -287,6 +287,13 @@ class TplinkC80Router(AbstractRouter):
         body = self._encrypt_body(text)
         self.request(1, 0, True, data=body)
 
+    def set_ewan_connect(self, enable: bool) -> None:
+        enable_string = f'enable {int(enable)}'
+        wan_link_request = "22|1,0,0"
+        text = f'id {wan_link_request}\r\n{enable_string}'
+        body = self._encrypt_body(text)
+        self.request(0, 0, True, data=body)
+
     def set_ipv4_dhcps(self, enable: bool) -> None:
         enable_string = f'enable {int(enable)}'
         text = f'id {RouterConstants.IPV4_DHCPS_REQUEST}\r\n{enable_string}'
