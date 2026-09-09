9ca9795252f26002b553537142441365e0e48a11
david9600 <david.c.arnold@icloud.com>
2026-09-09 14:04:30 +1000

    Incorporate e-wan in main query.

--- tplinkrouterc6v/client/mr.py
+++ tplinkrouterc6v/client/mr.py
@@ -100,7 +100,6 @@ class TPLinkMRClientBase(AbstractRouter):
         self._seq = None
         self._url_rsa_key = 'cgi/getParm'
         self._ipv6_support = True
-        self._wan_eth_support = True
         self._wan_usb_support = True
 
         self._encryption = EncryptionWrapperMR()
@@ -157,7 +156,8 @@ class TPLinkMRClientBase(AbstractRouter):
         acts = [
             self.ActItem(self.ActItem.GS, 'LAN_IP_INTF', attrs=['X_TP_MACAddress', 'IPInterfaceIPAddress']),
             self.ActItem(self.ActItem.GS, 'WAN_IP_CONN',
-                         attrs=['enable', 'MACAddress', 'externalIPAddress', 'defaultGateway', 'name']),
+                         attrs=['enable', 'MACAddress', 'externalIPAddress', 'defaultGateway', 'name',
+                                'connectionStatus', 'X_TP_IfName']),
             self.ActItem(self.ActItem.GL, 'LAN_WLAN', attrs=['enable', 'X_TP_Band']),
             self.ActItem(self.ActItem.GL, 'LAN_WLAN_GUESTNET', attrs=['enable', 'name']),
             self.ActItem(self.ActItem.GL, 'LAN_HOST_ENTRY', attrs=[
@@ -190,6 +190,8 @@ class TPLinkMRClientBase(AbstractRouter):
             status._wan_ipv4_addr = get_ip(item['externalIPAddress'])
             status._wan_ipv4_gateway = get_ip(item['defaultGateway'])
             status.conn_type = item.get('name', '')
+            if 'eth' in item.get('X_TP_IfName', ''):
+                status.ewan_connected = item.get('connectionStatus') == 'Connected'
 
         if values['2'].__class__ != list:
             status.wifi_2g_enable = bool(int(values['2']['enable']))
@@ -268,26 +270,6 @@ class TPLinkMRClientBase(AbstractRouter):
                     self._ipv6_support = False
             except Exception:
                 self._ipv6_support = False
-
-        # E-WAN connect status (for DHCP release/renew)
-        # Probe once and disable if not supported by router.
-        if self._wan_eth_support:
-            try:
-                wan_eth_acts = [
-                    self.ActItem(self.ActItem.GS, 'WAN_IP_CONN',
-                        attrs=['enable', 'connectionStatus', 'X_TP_IfName'])
-                ]
-                _, wan_eth_values = self.req_act(wan_eth_acts)
-                if wan_eth_values:
-                    for item in self._to_list(wan_eth_values):
-                        if not bool(int(item.get('enable'))) and wan_eth_values.__class__ == list:
-                            continue
-                        if 'eth' in item.get('X_TP_IfName', ''):
-                            status.ewan_connected = item.get('connectionStatus') == 'Connected'
-                else:
-                    self._wan_eth_support = False
-            except Exception:
-                self._wan_eth_support = False
 
         # For routers with USB modem support, get modem state string and backup enabled status.
         if self._wan_usb_support:
