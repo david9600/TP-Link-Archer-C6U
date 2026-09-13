7f33e25805ea657da695fd40e40b6d76e0d9e78a
david9600 <david.c.arnold@icloud.com>
2026-09-13 10:10:20 +1000

    Remove logging.

--- tplinkrouterc6v/client/mr.py
+++ tplinkrouterc6v/client/mr.py
@@ -175,7 +175,7 @@ class TPLinkMRClientBase(AbstractRouter):
         ]
         _, values = self.req_act(acts)
 
-        self._logger.info('values: %s', values)
+        # self._logger.info('values: %s', values)
 
         if values['0'].__class__ == list:
             values['0'] = values['0'][0]
