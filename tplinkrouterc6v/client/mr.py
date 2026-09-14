b4d2ea6bdd40ac694e946d40bf7da0c1dd9b6e03
david9600 <david.c.arnold@icloud.com>
2026-09-14 19:19:37 +1000

    Test non failover path.

--- tplinkrouterc6v/client/mr.py
+++ tplinkrouterc6v/client/mr.py
@@ -190,13 +190,13 @@ class TPLinkMRClientBase(AbstractRouter):
                 if int(item.get('enable')) == 1 or 'lte' in item.get('X_TP_IfName'):
                     wan_intf_count += 1
             self._logger.info('wan intf count: %s', wan_intf_count)
-            if wan_intf_count > 1:
+            if wan_intf_count > 2:
                 self._logger.info('support wan failover')
 
             else:
                 self._wan_failover_support = False
 
-        if not self._wan_failover_support:
+        else:
             for item in self._to_list(values.get('1')):
                 if int(item['enable']) == 0:
                     continue
