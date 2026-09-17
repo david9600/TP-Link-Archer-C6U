58027403d3d5e5d1c4b392c5f7d8d3432e97afc3
david9600 <david.c.arnold@icloud.com>
2026-09-17 19:03:30 +1000

    Test json=

--- tplinkrouterc6v/client/c6u.py
+++ tplinkrouterc6v/client/c6u.py
@@ -732,7 +732,7 @@ class TplinkBaseRouter(AbstractRouter, TplinkRequest):
         self.request(self._url_wan_ipv4_dynamic + '&operation=' + op, 'operation=' + op)
 
     def set_ipv4_dhcps(self, enable: bool) -> None:
-        data = self.request(self._url_ipv4_dhcps + '&operation=read', 'operation=read')
+        data = self.request(self._url_ipv4_dhcps, json='operation=read')
         payload = {
             'operation': 'write',
             'enable': 'on' if enable else 'off',
